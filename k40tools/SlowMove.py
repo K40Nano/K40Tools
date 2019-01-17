59ba50d241e8f974902d6bc518bd141ef5182490
tatarize <tatarize@gmail.com>
2019-01-17 02:17:02 -0800

    Added slowmove options

--- k40tools/SlowMove.py
+++ k40tools/SlowMove.py
@@ -3,8 +3,10 @@
 from __future__ import print_function
 
 import sys
+import time
+from math import sqrt
 
-from k40nano import NanoPlotter
+from k40nano import NanoPlotter, LaserSpeed
 
 
 def unit_convert(value):
@@ -20,20 +22,42 @@ def unit_convert(value):
 
 
 argv = sys.argv
-dx = 0
-dy = 0
-speed = 30
-if len(argv) == 2:
+
+if len(argv) == 1:
+    print("SlowMove <X> <Y> <MM_Per_Second> <Board> <Gear>")
+    exit(0)
+
+if len(argv) >= 2:
     dx = unit_convert(argv[1])
+else:
+    dx = 0
+
+if len(argv) >= 3:
+    dy = unit_convert(argv[2])
+else:
     dy = dx
-elif len(argv) == 3:
-    dx = unit_convert(argv[1])
-    dy = unit_convert(argv[2])
-elif len(argv) == 4:
-    dx = unit_convert(argv[1])
-    dy = unit_convert(argv[2])
+
+if len(argv) >= 4:
     speed = float(argv[3])
+else:
+    speed = 30
+
+if len(argv) >= 5:
+    board = str(argv[4])
+else:
+    board = "M2"
+
+if len(argv) >= 6:
+    gear = int(argv[5])
+else:
+    gear = None
+
 if dx != 0 or dy != 0:
+    t = time.time()
     with NanoPlotter() as plotter:
-        plotter.enter_compact_mode(speed)
+        speedcode = LaserSpeed.get_code_from_speed(speed, board=board, gear=gear)
+        plotter.enter_compact_mode(speedcode)
         plotter.move(dx, dy)
+    t = time.time() - t
+    d = sqrt(dx * dx + dy * dy)
+    print("Moved dx=%f dy=%f distance=%f in %f seconds at an overall rate: %fin/s" % (dx, dy, d, t, d / (1000 * t)))
